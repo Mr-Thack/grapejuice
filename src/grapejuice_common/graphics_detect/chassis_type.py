b8187c5de6aad22790fb85e23d38520189d5ab22
BrinkerVII <brinkervii@gmail.com>
2021-11-26 11:04:31 +0100

    Add chassis type detection

---
+++ src/grapejuice_common/graphics_detect/chassis_type.py
@@ -0,0 +1,60 @@
+from enum import Enum
+from pathlib import Path
+from typing import Optional
+
+
+class ChassisType(Enum):
+    Other = 1
+    Unknown = 2
+    Desktop = 3
+    LowProfileDesktop = 4
+    PizzaBox = 5
+    MiniTower = 6
+    Tower = 7
+    Portable = 8
+    Laptop = 9
+    Notebook = 10
+    HandHeld = 11
+    DockingStation = 12
+    AllInOne = 13
+    SubNotebook = 14
+    SpaceSaving = 15
+    LunchBox = 16
+    MainServerChassis = 17
+    ExpansionChassis = 18
+    SubChassis = 19
+    BusExpansionChassis = 20
+    PeripheralChassis = 21
+    RAIDChassis = 22
+    RackMountChassis = 23
+    SealedCasePC = 24
+    MultiSystemChassis = 25
+    CompactPCI = 26
+    AdvancedTCA = 27
+    Blade = 28
+    BladeEnclosure = 29
+    Tablet = 30
+    Convertible = 31
+    Detachable = 32
+    IoTGateway = 33
+    EmbeddedPC = 34
+    MiniPC = 35
+    StickPC = 36
+
+    @classmethod
+    def local_chassis_type(cls) -> "ChassisType":
+        dmi_path = Path("/sys/class/dmi/id/chassis_type")
+
+        if not dmi_path.exists():
+            return cls.Unknown
+
+        with dmi_path.open("r") as fp:
+            raw_chassis_type = fp.readline().strip()
+
+        try:
+            chassis_type_int = int(raw_chassis_type)
+
+        except ValueError:
+            return cls.Unknown
+
+        return cls(chassis_type_int)
