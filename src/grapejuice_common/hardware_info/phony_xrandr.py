d9a876cf7af9f4309142378969a41dc3e87feebd
BrinkerVII <brinkervii@gmail.com>
2021-12-21 04:02:39 +0100

    Ellipsize graphics card name in PhoneXRandR

--- src/grapejuice_common/hardware_info/phony_xrandr.py
+++ src/grapejuice_common/hardware_info/phony_xrandr.py
@@ -4,6 +4,29 @@ from grapejuice_common.hardware_info.lspci import LSPci, LSPciEntry
 from grapejuice_common.hardware_info.xrandr import IXRandR, XRandRProvider
 
 
+def _truncate_gpu_name(name: str, length: int = 50):
+    sep = " "
+    buffer = []
+
+    def format_buffer():
+        return sep.join(buffer)
+
+    def buffer_length():
+        return len(format_buffer())
+
+    for section in name.split(sep):
+        buffer.append(section)
+        if buffer_length() > length:
+            buffer.pop()
+            break
+
+    output_string = format_buffer()
+    if len(output_string) < len(name):
+        output_string += " ..."
+
+    return output_string
+
+
 def pci_entry_to_phony_xrandr_entry(index: int, entry: LSPciEntry):
     return XRandRProvider(
         index=index,
@@ -12,7 +35,7 @@ def pci_entry_to_phony_xrandr_entry(index: int, entry: LSPciEntry):
         crtcs=-1,
         outputs=1,
         associated_providers=-1,
-        name=entry.gpu_id_string
+        name=_truncate_gpu_name(entry.gpu_id_string)
     )
 
 
@@ -34,3 +57,13 @@ class PhonyXRandR(IXRandR):
     @property
     def providers(self) -> List[XRandRProvider]:
         return self._providers
+
+
+def main():
+    x = PhonyXRandR()
+    for provider in x.providers:
+        print(provider)
+
+
+if __name__ == '__main__':
+    main()
